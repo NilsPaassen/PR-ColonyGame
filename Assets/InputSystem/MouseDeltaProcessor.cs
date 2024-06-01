281d267f142e782d90620ab472a1289b05fc85c4
Majo <git@maxoverlack.dev>
2024-06-01 14:44:48 +0200

    #3 Fix mouse movement difference on macOS and mouse movement being frame rate dependent

---
+++ Assets/InputSystem/MouseDeltaProcessor.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+#if UNITY_EDITOR
+[InitializeOnLoad]
+#endif
+public class MouseDeltaProcessor : InputProcessor<Vector2>
+{
+    [Tooltip("Value when mouse is moved across the whole screen.")]
+    public float wholeScreenValue;
+
+    [Tooltip("Scale factor to multiply the mouse movement by on macOS.")]
+    public float macOsFactor;
+
+    public override Vector2 Process(Vector2 value, InputControl control)
+    {
+#if UNITY_STANDALONE_OSX || UNITY_EDITOR_OSX
+        value *= macOsFactor;
+#endif
+
+        return wholeScreenValue / (Mathf.Min(Screen.width, Screen.height) * Time.deltaTime) * value;
+    }
+
+#if UNITY_EDITOR
+    static MouseDeltaProcessor()
+    {
+        Initialize();
+    }
+#endif
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void Initialize()
+    {
+        InputSystem.RegisterProcessor<MouseDeltaProcessor>();
+    }
+}
