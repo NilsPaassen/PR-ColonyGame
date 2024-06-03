028a3d58e87130ba2a6545b2475a60a7ba72c8da
31467 <nils.paassen@hsrw.org>
2024-06-03 19:09:41 +0200

    Mines now output resources

--- Assets/Scripts/Building Specific/Mine.cs
+++ Assets/Scripts/Building Specific/Mine.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using VSCodeEditor;
 
 public class Mine : MonoBehaviour
 {
@@ -11,19 +12,46 @@ public class Mine : MonoBehaviour
 
     private int resourcesStored;
 
+    public int resourceLimit = 100;
+
     public String resourceType;
 
     // Start is called before the first frame update
     void Start() { }
 
     // Update is called once per frame
-    void Update() { }
+    void Update()
+    {
+        if (resourcesStored > 0 && outputConveyor.carriedObjects[2] == null)
+        {
+            OutputResource();
+        }
+    }
 
-    public void OnResourceProduced() { }
+    void OutputResource()
+    {
+        GameObject producedResource = new GameObject();
+        producedResource.tag = resourceType;
+        outputConveyor.carriedObjects[2] = producedResource;
+    }
+
+    public void ProduceResource(){
+        if (outputConveyor.carriedObjects[2] == null)
+        {
+            OutputResource();
+        }
+        else if (resourcesStored < resourceLimit)
+        {
+            resourcesStored++;
+        }
+
+        Invoke("ProduceResource",1f);
+    }
 
     public void OnBuild(String groundType)
     {
         resourceType = groundType;
         outputConveyor = outputConveyorObject.GetComponent<ConveyorBelt>();
+        Invoke("ProduceResource",1f);
     }
 }
