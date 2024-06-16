af571a0825ee2f539d99ead83382b209aa4576b1
31467 <nils.paassen@hsrw.org>
2024-06-16 22:07:26 +0200

    ImprovedShaders

---
+++ Assets/Scripts/TerrainGeneration/TerrainMeshGenerator.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+
+[RequireComponent(typeof(MeshFilter))]
+public class TerrainMeshGenerator : MonoBehaviour
+{
+}
